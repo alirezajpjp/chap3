9159c7c317e3faa9c10eb624092d583c555f0452
alirezajpjp <jahanparyalireza@yahoo.com>
2017-03-02 20:03:17 -0500

    firsst

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,18 +9,27 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-test "should get home" do
-    get static_pages_home_url
+
+  test "should get root" do
+    get root_url
     assert_response :success
- assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-assert_select "title", "Home | #{@base_title}"
+  end
+
+
+
+
+test "should get home" do
+    get root_path
+    assert_response :success
+ assert_select "title", "Home | #{@base_title}"
+
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
-assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
-    assert_select "title", "Help | #{@base_title}"
+assert_select "title", "Help | #{@base_title}"
+    
 
   end
 
@@ -28,18 +37,18 @@ assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
 
 
  test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-    assert_select "title", "About | #{@base_title}"
+assert_select "title", "About | #{@base_title}"
+   
 
   end
 
 test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
-assert_select "title", "contact | Ruby on Rails Tutorial Sample App"
-    assert_select "title", "contact | #{@base_title}"
+assert_select "title", "contact | #{@base_title}"
+   
 
   end
 
